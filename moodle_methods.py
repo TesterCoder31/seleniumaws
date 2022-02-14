ca5175e82cf854a99ffb772628645b8b152cd2a7
GameCoder22 <aaradhyayegi@gmail.com>
2022-02-14 12:45:12 -0800

    added

--- moodle_methods.py
+++ moodle_methods.py
@@ -5,10 +5,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import moodle_locators as locators
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.service import Service
+
 from selenium.webdriver.common.keys import Keys
-s = Service(executable_path='./chromedriver')
-driver = webdriver.Chrome(service=s)
+from selenium.webdriver.chrome.options import Options
+options = Options()
+
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(options=options)
 
 
 #make a full screen
