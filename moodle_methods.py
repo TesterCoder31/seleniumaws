ecaa3d62fc6539e87e26c5eec7271ab5aeb90d31
GameCoder22 <aaradhyayegi@gmail.com>
2022-02-14 19:42:42 -0800

    Added selenium scripts

--- moodle_methods.py
+++ moodle_methods.py
@@ -18,6 +18,7 @@ options.add_argument("start-maximized")
 options.add_argument("enable-automation")
 options.add_argument("--disable-infobars")
 options.add_argument("--disable-dev-shm-usage")
+
 driver = webdriver.Chrome(options=options)
 
 
