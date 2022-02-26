ef6d66aaf60d00f5377e15aaa61d38ac834ea8f0
GameCoder22 <aaradhyayegi@gmail.com>
2022-02-25 16:05:41 -0800

    Changed

--- moodle_locators.py
+++ moodle_locators.py
@@ -6,8 +6,8 @@ fake = Faker(locale='en_CA')
 moodle_url = 'http://52.39.5.126/'
 moodle_login_url = 'http://52.39.5.126/login/index.php'
 moodle_users_main_page = 'http://52.39.5.126/admin/user.php'
-moodle_username = 'GeetaYeginwar'
-moodle_password = 'Aaradhya@22'
+moodle_username = 'cctbuser'
+moodle_password = 'Moodle!123'
 moodle_dashboard_url = 'http://52.39.5.126/my/'
 new_username = fake.user_name()
 new_password = fake.password()
